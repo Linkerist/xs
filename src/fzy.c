7096589bd94d44633ac7fdfb23bc347f80c5844c
ZiChen Qian <Linkerist@163.com>
2018-03-14 22:38:14 +0800

    try to support fuzzy search.

---
+++ src/fzy.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
+#include "match.h"
+#include "tty.h"
+#include "choices.h"
+#include "options.h"
+#include "tty_interface.h"
+
+#include "../config.h"
+
+int main(int argc, char *argv[]) {
+	int ret = 0;
+
+	options_t options;
+	options_parse(&options, argc, argv);
+
+	choices_t choices;
+	choices_init(&choices, &options);
+	choices_fread(&choices, stdin);
+
+	if (options.benchmark) {
+		if (!options.filter) {
+			fprintf(stderr, "Must specify -e/--show-matches with --benchmark\n");
+			exit(EXIT_FAILURE);
+		}
+		for (int i = 0; i < options.benchmark; i++)
+			choices_search(&choices, options.filter);
+	} else if (options.filter) {
+		choices_search(&choices, options.filter);
+		for (size_t i = 0; i < choices_available(&choices); i++) {
+			if (options.show_scores)
+				printf("%f\t", choices_getscore(&choices, i));
+			printf("%s\n", choices_get(&choices, i));
+		}
+	} else {
+		/* interactive */
+		tty_t tty;
+		tty_init(&tty, options.tty_filename);
+
+		if (options.num_lines > choices.size)
+			options.num_lines = choices.size;
+
+		if (options.num_lines + 1 > tty_getheight(&tty))
+			options.num_lines = tty_getheight(&tty) - 1;
+
+		tty_interface_t tty_interface;
+		tty_interface_init(&tty_interface, &tty, &choices, &options);
+		ret = tty_interface_run(&tty_interface);
+	}
+
+	choices_destroy(&choices);
+
+	return ret;
+}
---
+++ src/options.h
@@ -0,0 +1,19 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H OPTIONS_H
+
+typedef struct {
+	int benchmark;
+	const char *filter;
+	const char *init_search;
+	const char *tty_filename;
+	int show_scores;
+	unsigned int num_lines;
+	unsigned int scrolloff;
+	const char *prompt;
+	unsigned int workers;
+} options_t;
+
+void options_init(options_t *options);
+void options_parse(options_t *options, int argc, char *argv[]);
+
+#endif
